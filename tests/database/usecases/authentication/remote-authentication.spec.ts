d423097c4da9c0bb6443b60dd95030de3c0782d7
Wesley Lira <wesleeylirac12@gmail.com>
2023-02-26 14:20:07 -0300

    test: ensure RemoteAuthentication returns an Account on success

--- tests/database/usecases/authentication/remote-authentication.spec.ts
+++ tests/database/usecases/authentication/remote-authentication.spec.ts
@@ -5,7 +5,10 @@ import { UnexpectedError } from "@/domain/errors/unexpected-error";
 import { AccountModel } from "@/domain/models/account-model";
 import { AuthenticationParams } from "@/domain/usecases/authentication";
 import { HttpPostClientSpy } from "@/tests/database/mocks";
-import { mockAuthentication } from "@/tests/domain/mocks/mock-authentication";
+import {
+  mockAccountModel,
+  mockAuthentication,
+} from "@/tests/domain/mocks/mock-account";
 
 import faker from "faker";
 
@@ -75,5 +78,16 @@ describe("RemoteAuthentication", () => {
     };
     const promise = sut.auth(mockAuthentication());
     await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+
+  test("Should return an AccountModel if HttpPostClient returns 200", async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    const httpResult = mockAccountModel();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult,
+    };
+    const account = await sut.auth(mockAuthentication());
+    expect(account).toEqual(httpResult);
   });
 });
