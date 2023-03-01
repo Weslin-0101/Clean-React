19a5c4115f23c1c3ae4bcfebfa599a1165ef87e2
Wesley Lira <wesleeylirac12@gmail.com>
2023-03-01 13:28:43 -0300

    ensure ValidationBuilder returns RequiredFieldValidation

--- src/validation/validators/index.ts
+++ src/validation/validators/index.ts
@@ -2,3 +2,4 @@ export * from "./required-field/required-field-validation";
 export * from "./email/email-validation";
 export * from "./min-lenght/min-lenght-validation";
 export * from "./validation-composite/validation-composite";
+export * from "./builder/validation-builder";
