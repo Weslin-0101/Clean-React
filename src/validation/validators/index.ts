b0c798e8eaebdda15397b47fd7136690b8dcec4f
Wesley Lira <wesleeylirac12@gmail.com>
2023-03-01 12:46:00 -0300

    feat: ensure ValidationComposite returns an error if any validation fails

--- src/validation/validators/index.ts
+++ src/validation/validators/index.ts
@@ -1,3 +1,4 @@
 export * from "./required-field/required-field-validation";
 export * from "./email/email-validation";
 export * from "./min-lenght/min-lenght-validation";
+export * from "./validation-composite/validation-composite";
