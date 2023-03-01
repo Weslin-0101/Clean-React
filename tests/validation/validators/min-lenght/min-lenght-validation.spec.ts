0f497e0e1b5d8cabc1f939948e51ab62a507122a
Wesley Lira <wesleeylirac12@gmail.com>
2023-03-01 11:50:25 -0300

    test: ensure MinLenghtValidation returns falsy if value is valid

--- tests/validation/validators/min-lenght/min-lenght-validation.spec.ts
+++ tests/validation/validators/min-lenght/min-lenght-validation.spec.ts
@@ -1,10 +1,22 @@
 import { InvalidFieldError } from "@/validation/errors";
 import { MinLenghtValidation } from "@/validation/validators";
 
+import faker from "faker";
+
+const makeSut = (): MinLenghtValidation => {
+  return new MinLenghtValidation("field", 8);
+};
+
 describe("MinLengthValidation", () => {
   test("Should return an error if value is invalid", () => {
-    const sut = new MinLenghtValidation("field", 8);
-    const error = sut.validate("123");
+    const sut = makeSut();
+    const error = sut.validate(faker.random.alphaNumeric(7));
     expect(error).toEqual(new InvalidFieldError("field"));
+  });
+
+  test("Should return falsy if value is valid", () => {
+    const sut = makeSut();
+    const error = sut.validate(faker.random.alphaNumeric(8));
+    expect(error).toBeFalsy();
   });
 });
