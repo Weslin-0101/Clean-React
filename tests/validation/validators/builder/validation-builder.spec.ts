068308e4140b60ef791fb644742473910714c0e0
Wesley Lira <wesleeylirac12@gmail.com>
2023-03-01 13:36:41 -0300

    test: ensure ValidationBuilder returns EmailValidation

--- tests/validation/validators/builder/validation-builder.spec.ts
+++ tests/validation/validators/builder/validation-builder.spec.ts
@@ -1,11 +1,17 @@
 import {
   RequiredFieldValidation,
   ValidationBuilder,
+  EmailValidation,
 } from "@/validation/validators";
 
 describe("ValidationBuilder", () => {
   test("Should return RequiredFieldValidation", () => {
     const validations = ValidationBuilder.field("any_field").required().build();
     expect(validations).toEqual([new RequiredFieldValidation("any_field")]);
+  });
+
+  test("Should return EmailValidation", () => {
+    const validations = ValidationBuilder.field("any_field").email().build();
+    expect(validations).toEqual([new EmailValidation("any_field")]);
   });
 });
