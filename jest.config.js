02ebc1cc7624b49f9869c9280c61652096ff2045
Wesley Lira <wesleeylirac12@gmail.com>
2023-03-01 17:52:24 -0300

    chore: add pages and files do not run coverage

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ module.exports = {
     "<rootDir>/src/**/*.{ts,tsx}",
     "!**/*.d.ts",
     "!<rootDir>/src/main/**/*",
+    "!<rootDir>/src/presentation/components/router/**/*",
+    "!<rootDir>/src/presentation/components/index.ts",
   ],
   coverageDirectory: "coverage",
   testEnvironment: "jsdom",
