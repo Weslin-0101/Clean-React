58c562e32198f43e44485dabed018e3a7bc6f042
Wesley Lira <wesleeylirac12@gmail.com>
2023-03-01 13:02:20 -0300

    test: ensure ValidationComposite retuns falsy if any validations not fails

--- tests/validation/validators/validation-composite/validation-composite.spec.ts
+++ tests/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,6 +1,8 @@
 import { ValidationComposite } from "@/validation/validators";
 import { FieldValidationSpy } from "@/tests/validation/mocks";
 
+import faker from "faker";
+
 type SutTypes = {
   sut: ValidationComposite;
   fieldValidationsSpy: FieldValidationSpy[];
@@ -21,9 +23,16 @@ const makeSut = (): SutTypes => {
 describe("ValidationComposite", () => {
   test("Should return error if any validation fails", () => {
     const { sut, fieldValidationsSpy } = makeSut();
-    fieldValidationsSpy[0].error = new Error("first_error_message");
-    fieldValidationsSpy[1].error = new Error("second_error_message");
+    const errorMessage = faker.random.words();
+    fieldValidationsSpy[0].error = new Error(errorMessage);
+    fieldValidationsSpy[1].error = new Error(faker.random.words());
     const error = sut.validate("any_field", "any_value");
-    expect(error).toBe("first_error_message");
+    expect(error).toBe(errorMessage);
+  });
+
+  test("Should return falsy if any validation not fails", () => {
+    const { sut } = makeSut();
+    const error = sut.validate("any_field", faker.random.words());
+    expect(error).toBeFalsy();
   });
 });
