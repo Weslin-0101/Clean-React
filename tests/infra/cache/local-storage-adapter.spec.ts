03aa7e1e141b69b08af98d3c8ed0f7f28dceb4ee
Wesley Lira <wesleeylirac12@gmail.com>
2023-03-02 16:56:50 -0300

    test: ensure LocalStorageAdapter calls localStorage with correct values

---
+++ tests/infra/cache/local-storage-adapter.spec.ts
@@ -0,0 +1,22 @@
+import { LocalStorageAdapter } from "@/infra/cache/local-storage-adapter";
+import "jest-localstorage-mock";
+
+import faker from "faker";
+
+const makeSut = (): LocalStorageAdapter => {
+  return new LocalStorageAdapter();
+};
+
+describe("LocalStorageAdapter", () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
+  test("Should call localStorage with correct values", async () => {
+    const sut = makeSut();
+    const key = faker.database.column();
+    const value = faker.random.word();
+    await sut.set(key, value);
+    expect(localStorage.setItem).toHaveBeenCalledWith(key, value);
+  });
+});
