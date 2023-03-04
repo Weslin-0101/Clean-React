4e1b5a6545de9e373d6f148f863078efe9e53cd3
Wesley Lira <wesleeylirac12@gmail.com>
2023-03-04 16:58:24 -0300

    test: ensure CompareFieldsValidations returns falsy if validations succeeds

--- tests/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ tests/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -8,6 +8,13 @@ describe("CompareFieldsValidation", () => {
   test("Should return error if compare is invalid", () => {
     const sut = makeSut("any_value");
     const error = sut.validate("other_value");
-    expect(error).toEqual(new InvalidFieldError(""));
+    expect(error).toEqual(new InvalidFieldError("field"));
+  });
+
+  test("Should return falsy if compare is valid", () => {
+    const valueToCompare = "any_value";
+    const sut = makeSut(valueToCompare);
+    const error = sut.validate(valueToCompare);
+    expect(error).toBeFalsy();
   });
 });
