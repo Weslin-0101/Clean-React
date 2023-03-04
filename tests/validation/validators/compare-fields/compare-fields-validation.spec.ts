ecf4715559845c00a91cbb3d2f1e3b89e3b462b4
Wesley Lira <wesleeylirac12@gmail.com>
2023-03-04 16:53:39 -0300

    test: ensure CompareFieldsValidation returns error if validation fails

---
+++ tests/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -0,0 +1,13 @@
+import { InvalidFieldError } from "@/validation/errors";
+import { CompareFieldsValidation } from "@/validation/validators";
+
+const makeSut = (valueToCompare: string): CompareFieldsValidation =>
+  new CompareFieldsValidation("field", valueToCompare);
+
+describe("CompareFieldsValidation", () => {
+  test("Should return error if compare is invalid", () => {
+    const sut = makeSut("any_value");
+    const error = sut.validate("other_value");
+    expect(error).toEqual(new InvalidFieldError(""));
+  });
+});
