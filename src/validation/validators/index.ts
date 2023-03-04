29a7644c65338923c6d7fa637cee1dd0a57267a2
Wesley Lira <wesleeylirac12@gmail.com>
2023-03-04 16:53:29 -0300

    feat: ensure CompareFieldsValidation returns error if validation fails

--- src/validation/validators/index.ts
+++ src/validation/validators/index.ts
@@ -3,3 +3,4 @@ export * from "./email/email-validation";
 export * from "./min-lenght/min-lenght-validation";
 export * from "./validation-composite/validation-composite";
 export * from "./builder/validation-builder";
+export * from "./compare-fields/compare-fields-validation";
