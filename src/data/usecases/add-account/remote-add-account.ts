2ad58ea50bd784ca9f5d14146b518e24df2d076a
Wesley Lira <wesleeylirac12@gmail.com>
2023-03-04 16:32:59 -0300

    feat: ensure RemoteAddAccount throws UnexpectedError if HttpPostClient returns 400

--- src/data/usecases/add-account/remote-add-account.ts
+++ src/data/usecases/add-account/remote-add-account.ts
@@ -19,10 +19,12 @@ export class RemoteAddAccount implements AddAccount {
     });
 
     switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:
+        return null;
       case HttpStatusCode.forbidden:
         throw new EmailInUseError();
       default:
-        return null;
+        throw new UnexpectedError();
     }
   }
 }
