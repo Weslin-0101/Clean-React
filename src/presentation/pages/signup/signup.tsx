18d249f460ab62fd46230578ab4d5a49b33982f9
Wesley Lira <wesleeylirac12@gmail.com>
2023-03-04 20:19:06 -0300

    feat: ensure SignUp presents error if AddAccount fails

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -45,16 +45,24 @@ const Signup: React.FC<Props> = ({ validation, addAccount }: Props) => {
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
         event.preventDefault();
-        if (state.isLoading || handleButtonError(state)) {
-            return
+        try {
+            if (state.isLoading || handleButtonError(state)) {
+                return
+            }
+            setState({ ...state, isLoading: true })
+            await addAccount.add({
+                name: state.name,
+                email: state.email,
+                password: state.password,
+                passwordConfirmations: state.passwordConfirmation,
+            })
+        } catch (error) {
+            setState({
+                ...state,
+                isLoading: false,
+                mainError: error.message
+            })
         }
-        setState({ ...state, isLoading: true })
-        await addAccount.add({
-            name: state.name,
-            email: state.email,
-            password: state.password,
-            passwordConfirmations: state.passwordConfirmation,
-        })
     }
     
     return (
