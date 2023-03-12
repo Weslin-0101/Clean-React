c2941fcf8823082ffe956e28ae39399b73ee21d4
Wesley Lira <wesleeylirac12@gmail.com>
2023-03-12 18:18:19 -0300

    feat: emsire RemoteLoadSurveyList throws UnexpectedError on 403

--- src/data/usecases/load-survey-list/remote-load-survey-list.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.ts
@@ -1,4 +1,5 @@
-import { HttpGetClient } from "@/data/protocols/http";
+import { HttpGetClient, HttpStatusCode } from "@/data/protocols/http";
+import { UnexpectedError } from "@/domain/errors";
 
 export class RemoteLoadSurveyList {
   constructor(
@@ -7,6 +8,21 @@ export class RemoteLoadSurveyList {
   ) {}
 
   async loadAll(): Promise<void> {
-    await this._httpGetClient.get({ url: this._url });
+    const httpResponse = await this._httpGetClient.get({ url: this._url });
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:
+        break;
+      default:
+        throw new UnexpectedError();
+    }
   }
+}
+
+export namespace RemoteLoadSurveyList {
+  export type Model = {
+    id: string;
+    question: string;
+    date: string;
+    didAnswer: boolean;
+  };
 }
