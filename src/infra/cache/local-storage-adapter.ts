a47f7b00741fb554a6986fc5224ca83684cc9058
Wesley Lira <wesleeylirac12@gmail.com>
2023-03-15 17:10:55 -0300

    feat: ensure LocalStorageAdapter calls removeItem if value is null

--- src/infra/cache/local-storage-adapter.ts
+++ src/infra/cache/local-storage-adapter.ts
@@ -2,7 +2,11 @@ import { SetStorage, GetStorage } from "@/data/protocols";
 
 export class LocalStorageAdapter implements SetStorage, GetStorage {
   async set(key: string, value: Object): Promise<void> {
-    localStorage.setItem(key, JSON.stringify(value));
+    if (value) {
+      localStorage.setItem(key, JSON.stringify(value));
+    } else {
+      localStorage.removeItem(key);
+    }
   }
 
   get(key: string) {
