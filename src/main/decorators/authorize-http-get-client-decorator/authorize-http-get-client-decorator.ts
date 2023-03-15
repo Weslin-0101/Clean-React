6ffc1f428cd318ed077eef931439a1edb5815f0e
Wesley Lira <wesleeylirac12@gmail.com>
2023-03-15 16:35:25 -0300

    feat: ensure AuthorizeHttpGetClientDecorator add headers if GetStorage is valid

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
@@ -12,7 +12,14 @@ export class AuthorizeHttpGetClientDecorator implements HttpGetClient {
   ) {}
 
   async get(params: HttpGetParams): Promise<HttpResponse<any>> {
-    this._getStorage.get("account");
+    const account = this._getStorage.get("account");
+    if (account?.accessToken) {
+      Object.assign(params, {
+        headers: {
+          "x-access-token": account.accessToken,
+        },
+      });
+    }
     await this._httpGetClient.get(params);
     return null;
   }
