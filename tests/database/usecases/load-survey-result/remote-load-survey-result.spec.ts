f223cfe453dc335cabf847ff4dc2bdee5c5e39f9
Wesley Lira <wesleeylirac12@gmail.com>
2023-03-17 11:34:57 -0300

    test: ensure RemoteLoadSurveyResult throws AccessDeniedError on 403

--- tests/database/usecases/load-survey-result/remote-load-survey-result.spec.ts
+++ tests/database/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -1,4 +1,6 @@
+import { HttpStatusCode } from "@/data/protocols";
 import { RemoteLoadSurveyResult } from "@/data/usecases";
+import { AccessDeniedError } from "@/domain/errors";
 import { HttpGetClientSpy } from "@/tests/database/mocks";
 
 type SutTypes = {
@@ -21,5 +23,14 @@ describe("RemoteLoadSurveyResult Usecase", () => {
     const { sut, httpGetClientSpy } = makeSut(url);
     await sut.load();
     expect(httpGetClientSpy.url).toBe(url);
+  });
+
+  test("Should throw AccessDeniedError if HttpGetClient returns 403", async () => {
+    const { sut, httpGetClientSpy } = makeSut();
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden,
+    };
+    const promise = sut.load();
+    await expect(promise).rejects.toThrow(new AccessDeniedError());
   });
 });
