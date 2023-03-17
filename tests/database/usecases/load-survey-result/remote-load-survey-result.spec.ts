2c38e97926c1b49a0c9e8e456cffe6949591c57b
Wesley Lira <wesleeylirac12@gmail.com>
2023-03-17 11:27:59 -0300

    refactor: move sut creation to a factory method

--- tests/database/usecases/load-survey-result/remote-load-survey-result.spec.ts
+++ tests/database/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -1,11 +1,24 @@
 import { RemoteLoadSurveyResult } from "@/data/usecases";
 import { HttpGetClientSpy } from "@/tests/database/mocks";
 
+type SutTypes = {
+  sut: RemoteLoadSurveyResult;
+  httpGetClientSpy: HttpGetClientSpy;
+};
+
+const makeSut = (url = "any_url"): SutTypes => {
+  const httpGetClientSpy = new HttpGetClientSpy();
+  const sut = new RemoteLoadSurveyResult(url, httpGetClientSpy);
+  return {
+    sut,
+    httpGetClientSpy,
+  };
+};
+
 describe("RemoteLoadSurveyResult Usecase", () => {
   test("Should call HttpGetClient with correct URL", async () => {
     const url = "any_url";
-    const httpGetClientSpy = new HttpGetClientSpy();
-    const sut = new RemoteLoadSurveyResult(url, httpGetClientSpy);
+    const { sut, httpGetClientSpy } = makeSut(url);
     await sut.load();
     expect(httpGetClientSpy.url).toBe(url);
   });
